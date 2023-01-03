8d45cd541ee07dc8153ce60406f6889932707a85
krishna.lolage <krish.lolage@gmail.com>
2023-01-03 21:30:57 +0530

    Restructured files and directories

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DragonMedicalSpeechKitLib",
     platforms: [
-        .iOS("14.0")
+        .iOS(.v14)
         ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -15,16 +15,11 @@ let package = Package(
             targets: ["DragonMedicalSpeechKit"]
         )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DragonMedicalSpeechKitBundle",
-//            dependencies: ["DragonMedicalSpeechKit"],
             resources: [.copy("Resources/DragonMedicalSpeechKit.bundle")]
         ),
 //        .binaryTarget(
@@ -34,7 +29,7 @@ let package = Package(
 //        ),
         .binaryTarget(
                     name: "DragonMedicalSpeechKit",
-                    path: "SDK/DragonMedicalSpeechKit.zip"
+                    path: "Sources/SDK/DragonMedicalSpeechKit.zip"
                 ),
     ]
 )
