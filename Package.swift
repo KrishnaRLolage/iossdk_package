0a5eaf23dcc4b3c0fb281971ee15637bb3581f20
krishna.lolage <krish.lolage@gmail.com>
2023-01-04 22:49:07 +0530

    trial 4

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DragonMedicalSpeechKit",
-            targets: ["DragonMedicalSpeechKit"]
+            targets: ["DragonMedicalSpeechKit", "DragonMedicalSpeechKitBundle"]
         )
     ],
     dependencies: [
@@ -27,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DragonMedicalSpeechKitBundle",
-            dependencies: [.target(name: "DragonMedicalSpeechKit"), .target(name: "DragonMedicalSpeechKitSDK")],
+            dependencies: [.target(name: "DragonMedicalSpeechKitSDK")],
             resources: [.copy("assets/DragonMedicalSpeechKit.bundle")]
         ),
         .target(
