a91ea129a88fbc06af52f09a05a7e095787b15d3
krishna.lolage <krish.lolage@gmail.com>
2023-01-04 20:58:51 +0530

    Corrected the dependancies and target in manifest

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DragonMedicalSpeechKit",
-            targets: ["DragonMedicalSpeechKit", "DragonMedicalSpeechKitBundle"]
+            targets: ["DragonMedicalSpeechKit"]
         )
     ],
     dependencies: [
@@ -27,7 +27,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DragonMedicalSpeechKitBundle",
-            dependencies: ["YbridOpus"],
+            dependencies: [
+                .target(name: "DragonMedicalSpeechKit"),
+                .product(name: "YbridOpus", package: "YbridOpus")
+            ],
             resources: [.copy("Resources/DragonMedicalSpeechKit.bundle")]
         ),
 //        .binaryTarget(
