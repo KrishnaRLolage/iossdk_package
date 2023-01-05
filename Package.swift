79f85c686b3f2bbb60ca6f31622a205a24ad6cc4
krishna.lolage <krish.lolage@gmail.com>
2023-01-05 11:40:46 +0530

    trial 123

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DragonMedicalSpeechKit",
-            targets: ["DragonMedicalSpeechKit", "opus"]
+            targets: ["DragonMedicalSpeechKitLib"]
         )
     ],
     dependencies: [
@@ -25,16 +25,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "DragonMedicalSpeechKitLib",
+            dependencies: [.target(name: "DragonMedicalSpeechKit"), .product(name: "YbridOpus", package: "YbridOpus")],
+            path: "Sources/SDK",
+            resources: [.process("test.txt")]
+        ),
         .binaryTarget(
                     name: "DragonMedicalSpeechKit",
                     path: "Sources/SDK/DragonMedicalSpeechKit.zip"
-                ),
-        .target(
-            name: "opus",
-            dependencies: [.product(name: "YbridOpus", package: "YbridOpus")],
-            path: "Sources/SDK",
-            resources: [.process("test.txt")]
-        )
+                )
     ]
 )
 
