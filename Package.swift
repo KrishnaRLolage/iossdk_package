90f94f4edd68ba1bf1d5e3189f07fbc39c0030f8
krishna.lolage <krish.lolage@gmail.com>
2023-01-05 16:59:47 +0530

    trial 321

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DragonMedicalSpeechKit",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v14)
         ],
@@ -12,17 +13,16 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DragonMedicalSpeechKit",
-            targets: ["DragonMedicalSpeechKitBundle"]
+            targets: ["abc", "DragonMedicalSpeechKit"]
         )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DragonMedicalSpeechKitBundle",
-            dependencies: [.target(name: "DragonMedicalSpeechKit")],
-            path: "Sources/SDK/abc/",
-            resources: [.copy("")]
+            name: "abc",
+            path: "Sources/SDK",
+            resources: [.copy("abc/Correction.storyboardc")]
         ),
         .binaryTarget(
                     name: "DragonMedicalSpeechKit",
