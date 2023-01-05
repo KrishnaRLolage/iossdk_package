837095555d2f23d88d54812119f67521ba8f8ce9
krishna.lolage <krish.lolage@gmail.com>
2023-01-05 15:48:53 +0530

    renamed

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "DragonMedicalSpeechKitSDK",
+    name: "DragonMedicalSpeechKit",
     platforms: [
         .iOS(.v14)
         ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DragonMedicalSpeechKitSDK",
+            name: "DragonMedicalSpeechKit",
             targets: ["DragonMedicalSpeechKitLib", "DragonMedicalSpeechKit"]
         )
     ],
