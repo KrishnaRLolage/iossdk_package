dcf58c52b35f724ca55debe0116d3e33d9ef4611
krishna.lolage <krish.lolage@gmail.com>
2023-01-05 17:06:18 +0530

    trial 321 321

--- Package.swift
+++ Package.swift
@@ -13,16 +13,16 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DragonMedicalSpeechKit",
-            targets: ["abc", "DragonMedicalSpeechKit"]
+            targets: ["DragonMedicalSpeechKitBundle", "DragonMedicalSpeechKit"]
         )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "abc",
+            name: "DragonMedicalSpeechKitBundle",
             path: "Sources/SDK",
-            resources: [.copy("abc/Correction.storyboardc")]
+            resources: [.copy("DragonMedicalSpeechKitBundle/")]
         ),
         .binaryTarget(
                     name: "DragonMedicalSpeechKit",
